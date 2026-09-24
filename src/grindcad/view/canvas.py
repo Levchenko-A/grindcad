1f92830abe9161b126909388ae9d6ff8dccf6ce2
Andrii Levchenko <levchenkoandrii91@gmail.com>
2026-09-24 20:38:01 +0200

    Draw coordinate axes in CAD canvas

--- src/grindcad/view/canvas.py
+++ src/grindcad/view/canvas.py
@@ -1,3 +1,4 @@
+from PySide6.QtGui import QPainter, QPen
 from PySide6.QtWidgets import QWidget
 
 
@@ -6,3 +7,25 @@ class Canvas(QWidget):
 
     def __init__(self, parent=None):
         super().__init__(parent)
+
+    def paintEvent(self, event):
+        painter = QPainter(self)
+
+        pen = QPen()
+        pen.setWidth(1)
+
+        painter.setPen(pen)
+
+        width = self.width()
+        height = self.height()
+
+        center_x = width // 2
+        center_y = height // 2
+
+        # X axis
+        painter.drawLine(0, center_y, width, center_y)
+
+        # Y axis
+        painter.drawLine(center_x, 0, center_x, height)
+
+        painter.end()
