58e38928a09e0a8fda1f07687d23d28a89c26c88
Andrii Levchenko <levchenkoandrii91@gmail.com>
2026-09-24 20:43:41 +0200

    Add world coordinate grid

--- src/grindcad/view/canvas.py
+++ src/grindcad/view/canvas.py
@@ -13,33 +13,46 @@ class Canvas(QWidget):
     def paintEvent(self, event):
         painter = QPainter(self)
 
-        pen = QPen()
-        pen.setWidth(1)
-        painter.setPen(pen)
-
         width = self.width()
         height = self.height()
 
-        # Move the origin to the center of the canvas.
+        # Move origin to center of canvas.
         painter.translate(width / 2, height / 2)
 
-        # Flip the Y axis so positive Y points upward.
+        # Flip Y axis so positive Y points upward.
         painter.scale(self.scale, -self.scale)
 
-        # Draw X axis.
-        painter.drawLine(
-            -width,
-            0,
-            width,
-            0,
-        )
-
-        # Draw Y axis.
-        painter.drawLine(
-            0,
-            -height,
-            0,
-            height,
-        )
+        self.draw_grid(painter, width, height)
+        self.draw_axes(painter, width, height)
 
         painter.end()
+
+    def draw_grid(self, painter, width, height):
+        pen = QPen()
+        pen.setWidth(0)
+        painter.setPen(pen)
+
+        grid_size = 50
+
+        x_min = -width
+        x_max = width
+
+        y_min = -height
+        y_max = height
+
+        for x in range(x_min, x_max + 1, grid_size):
+            painter.drawLine(x, y_min, x, y_max)
+
+        for y in range(y_min, y_max + 1, grid_size):
+            painter.drawLine(x_min, y, x_max, y)
+
+    def draw_axes(self, painter, width, height):
+        pen = QPen()
+        pen.setWidth(2)
+        painter.setPen(pen)
+
+        # X axis
+        painter.drawLine(-width, 0, width, 0)
+
+        # Y axis
+        painter.drawLine(0, -height, 0, height)
